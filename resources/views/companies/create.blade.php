0d3659339a7d7ea83f0af83b0666f730845ae1e8
visualcodemaster <visualcodemaster@gmail.com>
2019-07-21 23:54:19 +0500

    Second commit with the removal of some typos

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -45,10 +45,9 @@
                 </div>
 
 
-        </div>
+
 
         </form>
-    </div>
-
+        </div>
     </div>
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
     <div class="container">
-        <h1>Welcome to Visual Spark</h1>
+        <h1>Welcome to Laravel Beginner Template</h1>
     </div>
 
 @stop
